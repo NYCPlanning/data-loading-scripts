231eafb4814c7fd48f7d63584461a5bcd1445bd3
Hannah Kates <hbk254@nyu.edu>
2017-02-26 17:18:07 -0500

    new facility dataset updates

--- datasets/sbs_facilities_workforce1/create.sql
+++ datasets/sbs_facilities_workforce1/create.sql
@@ -1,9 +1,18 @@
 -- create table to load csv from the nyc open data portal
 DROP TABLE IF EXISTS sbs_facilities_workforce1;
 CREATE TABLE sbs_facilities_workforce1 (
-	Name_of_Center text,
-	Address_Line_1 text,
+	Name text,
+	Address_Number text,
+	Street text,
+	Street_Address_1 text,
+	Street_Address_2 text,
 	City text,
+	Borough text,
 	State text,
-	Zip_Code text
+	ZIP text,
+	Location_Type text,
+	Latitude text,
+	Longitude text,
+	X text,
+	Y text
 )
