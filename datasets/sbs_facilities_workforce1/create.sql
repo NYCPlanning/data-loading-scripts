8d42e95b795dfdfc1839a73c773afe176254877f
Hannah Kates <hbk254@nyu.edu>
2017-05-23 17:16:25 -0400

    new sbs dataset

--- datasets/sbs_facilities_workforce1/create.sql
+++ datasets/sbs_facilities_workforce1/create.sql
@@ -2,17 +2,22 @@
 DROP TABLE IF EXISTS sbs_facilities_workforce1;
 CREATE TABLE sbs_facilities_workforce1 (
 	Name text,
-	Address_Number text,
+	Hours text,
+	HouseNumber text,
 	Street text,
-	Street_Address_1 text,
 	Street_Address_2 text,
 	City text,
 	Borough text,
 	State text,
-	ZIP text,
+	Postcode text,
+	Details text,
 	Location_Type text,
 	Latitude text,
 	Longitude text,
-	X text,
-	Y text
+	BIN text,
+	BBL text,
+	NTA text,
+	Council_District text,
+	Census_Tract text,
+	Community_Board text
 )
