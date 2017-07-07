f617037e7a50f73a0f20434cc198fd47ada17747
Hannah Kates <hannah.b.kates@gmail.com>
2017-07-07 14:30:58 -0400

    Update after.sql

--- datasets/hhs_facilities_financialscontracts/after.sql
+++ datasets/hhs_facilities_financialscontracts/after.sql
@@ -1,2 +1,2 @@
 ALTER TABLE hhs_facilities_financialscontracts ADD COLUMN hash text;
-UPDATE hhs_facilities_financialscontracts SET hash = array_to_string(ARRAY_AGG(md5(CAST((hhs_facilities_financialscontracts.*) AS text))),;);
+UPDATE hhs_facilities_financialscontracts SET hash = array_to_string(ARRAY_AGG(md5(CAST((hhs_facilities_financialscontracts.*) AS text))));
