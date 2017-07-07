7e45dbaa81f93f9e91e8fb29dfda5047a9b5012d
Hannah Kates <hannah.b.kates@gmail.com>
2017-07-07 16:50:38 -0400

    fixed hhs syntax errors

--- datasets/hhs_facilities_proposals/after.sql
+++ datasets/hhs_facilities_proposals/after.sql
@@ -1,2 +1,2 @@
 ALTER TABLE hhs_facilities_proposals ADD COLUMN hash text;
-UPDATE hhs_facilities_proposals SET hash = array_to_string(ARRAY_AGG(md5(CAST((hhs_facilities_proposals.*) AS text))));
+UPDATE hhs_facilities_proposals SET hash = md5(CAST((hhs_facilities_proposals.*) AS text));
