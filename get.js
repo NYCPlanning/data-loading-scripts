c4f16e2ffe70cf642435548ae02b1e79ae8ba717
Jonathan Pichot <jonathan@pichot.us>
2017-08-04 16:23:48 -0400

    get changes

--- get.js
+++ get.js
@@ -1,96 +1,102 @@
-module.exports = function(dataset) {
-  //load dependencies
-  var download = require('download-file'),
-    unzip = require('unzip'),
-    fs = require('fs-extra'),
-    Mustache = require('mustache')
-    FTP = require('ftp-get')
+// Args passed in
+var dataset = process.argv[2];
+var argv = require('minimist')(process.argv.slice(3));
 
-  //get FTP credentials
-  var ftpAuth = require('./ftpauth.js')
+var download_dir = argv.download_dir + "/";
 
-  var dirPath = './datasets/' + dataset;
+ftpAuth = {
+  username: argv.ftp_user,
+  password: argv.ftp_pass
+}
 
-  //get the configuration for this dataset from its data.json file
-  var config = require(dirPath + '/data.json')
+//load dependencies
+var download = require('download-file'),
+  unzip = require('unzip'),
+  fs = require('fs-extra'),
+  Mustache = require('mustache')
+  FTP = require('ftp-get')
 
-  //use defined saveFile name or usr the URL
-  config.saveFile = config.saveFile || getFilename(config.url);
-  config.writePath = './temp/' + dataset;
+var dirPath = './datasets/' + dataset;
 
-  console.log('Downloading dataset ' + dataset + ' from ' + config.url)
+//get the configuration for this dataset from its data.json file
+var config = require(dirPath + '/data.json')
 
-  //check if HTTP or FTP
-  if(config.url.indexOf('http') > -1) {
-    return getHTTP(config);
-  } else if (config.url.indexOf('ftp') > -1) {
-    return getFTP(config);
-  }
-      
-  function getHTTP(config) {
-    return new Promise(
-      function(resolve, reject) {
-          
+//use defined saveFile name or usr the URL
+config.saveFile = config.saveFile || getFilename(config.url);
+config.writePath = download_dir + dataset;
 
-          var options = {
-            directory: config.writePath,
-            filename: config.saveFile
-          }
-          download(config.url, options, function(err){
-          if (err) throw err
+console.log('Downloading dataset ' + dataset + ' from ' + config.url)
 
+//check if HTTP or FTP
+if(config.url.indexOf('http') > -1) {
+  return getHTTP(config);
+} else if (config.url.indexOf('ftp') > -1) {
+  return getFTP(config);
+}
+
+function getHTTP(config) {
+  return new Promise(
+    function(resolve, reject) {
+
+
+        var options = {
+          directory: config.writePath,
+          filename: config.saveFile
+        }
+        download(config.url, options, function(err){
+        if (err) throw err
+
+        console.log('Saved file to ' + config.writePath + '/' + config.saveFile)
+        var ext = getExtension(config.saveFile);
+        if (ext=='zip') {
+
+          console.log('Unzipping ' + config.writePath + '/' + config.saveFile)
+          var stream = fs.createReadStream(config.writePath + '/' + config.saveFile)
+            .pipe(unzip.Extract({ path: config.writePath }));
+
+          stream.on('close', function() {
+            resolve();
+          })
+        } else {
+          resolve();
+        }
+      })
+    }
+  )
+}
+
+
+function getFTP(config) {
+  return new Promise(
+    function(resolve, reject) {
+
+      config.url=Mustache.render(config.url, ftpAuth)
+      fs.emptyDirSync(config.writePath);
+
+      FTP.get(config.url, config.writePath + '/' + config.saveFile, function (err, res) {
+        if(!err) {
           console.log('Saved file to ' + config.writePath + '/' + config.saveFile)
           var ext = getExtension(config.saveFile);
           if (ext=='zip') {
-
             console.log('Unzipping ' + config.writePath + '/' + config.saveFile)
-            var stream = fs.createReadStream(config.writePath + '/' + config.saveFile)
+            fs.createReadStream(config.writePath + '/' + config.saveFile)
               .pipe(unzip.Extract({ path: config.writePath }));
-            
-            stream.on('close', function() {
-              resolve();
-            })
+            resolve();
           } else {
             resolve();
           }
-        }) 
-      }
-    )
-  }
+        }
+      })
+    }
+  );
+}
 
+// get everything to the right of the last /... this should be the filename. But maybe not.
+function getFilename(url) {
+    return url.split('/').pop();
+}
 
-  function getFTP(config) {
-    return new Promise(
-      function(resolve, reject) {
-
-        config.url=Mustache.render(config.url, ftpAuth)
-        fs.emptyDirSync(config.writePath);
-
-        FTP.get(config.url, config.writePath + '/' + config.saveFile, function (err, res) {
-          if(!err) {
-            console.log('Saved file to ' + config.writePath + '/' + config.saveFile)
-            var ext = getExtension(config.saveFile);
-            if (ext=='zip') {
-              console.log('Unzipping ' + config.writePath + '/' + config.saveFile)
-              fs.createReadStream(config.writePath + '/' + config.saveFile)
-                .pipe(unzip.Extract({ path: config.writePath }));
-              resolve();
-            } else {
-              resolve();
-            }
-          }
-        })
-      }
-    );
-  }
-
-  // get everything to the right of the last /... this should be the filename. But maybe not.
-  function getFilename(url) {
-      return url.split('/').pop();
-  }
-
-  //get file extension from filename
-  function getExtension(filename) {
-      return filename.split('.').pop();
-  }
+//get file extension from filename
+function getExtension(filename) {
+    return filename.split('.').pop();
 }
