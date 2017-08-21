27dab993d74e57e05281a356d8513b744a400f1c
user <Amanda.Doyle.GIS@gmail.com>
2017-08-21 15:49:53 -0400

    changing ummapped to unmapped

---
+++ datasets/dcp_cpdb_agencyverified_unmapped/after.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS dcp_cpdb_agencyverified_unmapped;
+CREATE TABLE dcp_cpdb_agencyverified_unmapped AS 
+SELECT * FROM ACS_unmapped
+UNION ALL
+SELECT * FROM DOC_unmapped
+UNION ALL
+SELECT * FROM DOHMH_unmapped
+UNION ALL
+SELECT * FROM DOT_unmapped
+UNION ALL
+SELECT * FROM HHC_unmapped
+UNION ALL
+SELECT * FROM HRA_unmapped
+UNION ALL
+SELECT * FROM OCA_unmapped;
+
+DROP TABLE ACS_unmapped;
+DROP TABLE DOC_unmapped;
+DROP TABLE DOHMH_unmapped;
+DROP TABLE DOT_unmapped;
+DROP TABLE HHC_unmapped;
+DROP TABLE HRA_unmapped;
+DROP TABLE OCA_unmapped;
---
+++ datasets/dcp_cpdb_agencyverified_unmapped/copy.sql
@@ -0,0 +1,9 @@
+\COPY ACS_unmapped FROM './temp/dcp_cpdb_agencyverified_unmapped/ACS_unmapped.csv' CSV HEADER;
+\COPY DOC_unmapped FROM './temp/dcp_cpdb_agencyverified_unmapped/DOC_unmapped.csv' CSV HEADER;
+\COPY DOHMH_unmapped FROM './temp/dcp_cpdb_agencyverified_unmapped/DOHMH_unmapped.csv' CSV HEADER;
+\COPY DOT_unmapped FROM './temp/dcp_cpdb_agencyverified_unmapped/DOT_unmapped.csv' CSV HEADER;
+\COPY HHC_unmapped FROM './temp/dcp_cpdb_agencyverified_unmapped/HHC_unmapped.csv' CSV HEADER;
+\COPY HRA_unmapped FROM './temp/dcp_cpdb_agencyverified_unmapped/HRA_unmapped.csv' CSV HEADER;
+\COPY OCA_unmapped FROM './temp/dcp_cpdb_agencyverified_unmapped/OCA_unmapped.csv' CSV HEADER;
+
+
