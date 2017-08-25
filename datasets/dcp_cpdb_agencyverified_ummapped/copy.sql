bbf29633fbbdb927587b6c4074dd6055834a0afa
user <Amanda.Doyle.GIS@gmail.com>
2017-08-25 16:21:33 -0400

    changing agency verfied data loading scripts

--- datasets/dcp_cpdb_agencyverified_ummapped/copy.sql
+++
@@ -1,9 +0,0 @@
-\COPY ACS_unmapped FROM './temp/dcp_cpdb_agencyverified_ummapped/ACS_unmapped.csv' CSV HEADER;
-\COPY DOC_unmapped FROM './temp/dcp_cpdb_agencyverified_ummapped/DOC_unmapped.csv' CSV HEADER;
-\COPY DOHMH_unmapped FROM './temp/dcp_cpdb_agencyverified_ummapped/DOHMH_unmapped.csv' CSV HEADER;
-\COPY DOT_unmapped FROM './temp/dcp_cpdb_agencyverified_ummapped/DOT_unmapped.csv' CSV HEADER;
-\COPY HHC_unmapped FROM './temp/dcp_cpdb_agencyverified_ummapped/HHC_unmapped.csv' CSV HEADER;
-\COPY HRA_unmapped FROM './temp/dcp_cpdb_agencyverified_ummapped/HRA_unmapped.csv' CSV HEADER;
-\COPY OCA_unmapped FROM './temp/dcp_cpdb_agencyverified_ummapped/OCA_unmapped.csv' CSV HEADER;
-
-
--- datasets/dcp_cpdb_agencyverified_unmapped/copy.sql
+++
@@ -1,9 +0,0 @@
-\COPY acs_unmapped FROM './temp/dcp_cpdb_agencyverified_unmapped/ACS_unmapped.csv' CSV HEADER;
-\COPY doc_unmapped FROM './temp/dcp_cpdb_agencyverified_unmapped/DOC_unmapped.csv' CSV HEADER;
-\COPY dohmh_unmapped FROM './temp/dcp_cpdb_agencyverified_unmapped/DOHMH_unmapped.csv' CSV HEADER;
-\COPY dot_unmapped FROM './temp/dcp_cpdb_agencyverified_unmapped/DOT_unmapped.csv' CSV HEADER;
-\COPY hhc_unmapped FROM './temp/dcp_cpdb_agencyverified_unmapped/HHC_unmapped.csv' CSV HEADER;
-\COPY hra_unmapped FROM './temp/dcp_cpdb_agencyverified_unmapped/HRA_unmapped.csv' CSV HEADER;
-\COPY oca_unmapped FROM './temp/dcp_cpdb_agencyverified_unmapped/OCA_unmapped.csv' CSV HEADER;
-
-
