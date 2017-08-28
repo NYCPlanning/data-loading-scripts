ed2ee7e60b820217b5e974ff8ab49dce38d8b92f
user <Amanda.Doyle.GIS@gmail.com>
2017-08-28 16:24:58 -0400

    updating projection for NYPD

--- datasets/dcp_cpdb_agencyverified/copy.sql
+++ datasets/dcp_cpdb_agencyverified/copy.sql
@@ -3,11 +3,11 @@
 \COPY doc_unmapped FROM './temp/dcp_cpdb_agencyverified/DOC_unmapped.csv' CSV HEADER;
 \COPY dohmh_mapped FROM './temp/dcp_cpdb_agencyverified/DOHMH_mapped.csv' CSV HEADER;
 \COPY dohmh_unmapped FROM './temp/dcp_cpdb_agencyverified/DOHMH_unmapped.csv' CSV HEADER;
-\COPY dot_mapped FROM './temp/dcp_cpdb_agencyverified/DOT_unmapped.csv' CSV HEADER;
+\COPY dot_mapped FROM './temp/dcp_cpdb_agencyverified/DOT_mapped.csv' CSV HEADER;
 \COPY dot_unmapped FROM './temp/dcp_cpdb_agencyverified/DOT_unmapped.csv' CSV HEADER;
-\COPY hhc_mapped FROM './temp/dcp_cpdb_agencyverified/HHC_unmapped.csv' CSV HEADER;
+\COPY hhc_mapped FROM './temp/dcp_cpdb_agencyverified/HHC_mapped.csv' CSV HEADER;
 \COPY hhc_unmapped FROM './temp/dcp_cpdb_agencyverified/HHC_unmapped.csv' CSV HEADER;
-\COPY hra_mapped FROM './temp/dcp_cpdb_agencyverified/HRA_unmapped.csv' CSV HEADER;
+\COPY hra_mapped FROM './temp/dcp_cpdb_agencyverified/HRA_mapped.csv' CSV HEADER;
 \COPY hra_unmapped FROM './temp/dcp_cpdb_agencyverified/HRA_unmapped.csv' CSV HEADER;
-\COPY oca_mapped FROM './temp/dcp_cpdb_agencyverified/OCA_unmapped.csv' CSV HEADER;
+\COPY oca_mapped FROM './temp/dcp_cpdb_agencyverified/OCA_mapped.csv' CSV HEADER;
 \COPY oca_unmapped FROM './temp/dcp_cpdb_agencyverified/OCA_unmapped.csv' CSV HEADER;
