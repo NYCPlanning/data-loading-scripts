2d0f19e4adcc7070d597efa5a87ba8e82be88555
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2017-09-25 11:21:43 -0400

    fixing error with after sql

--- datasets/usdot_facilities_airports/after.sql
+++ datasets/usdot_facilities_airports/after.sql
@@ -1,2 +1,2 @@
-ALTER TABLE public.usdot_facilities_airports ADD COLUMN hash text;
-UPDATE public.usdot_facilities_airports SET hash = md5(CAST((public.usdot_facilities_airports.*) AS text));
+ALTER TABLE usdot_facilities_airports ADD COLUMN hash text;
+UPDATE usdot_facilities_airports SET hash = md5(CAST((usdot_facilities_airports.*) AS text));
