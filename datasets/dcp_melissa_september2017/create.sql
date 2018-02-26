7287693070fa8ec2f32fd7d447ff26c76642d412
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2018-02-26 11:59:27 -0500

    adding loading scripts for melissa data

--- datasets/dcp_melissa_september2017/create.sql
+++ datasets/dcp_melissa_september2017/create.sql
@@ -1,25 +1,13 @@
 -- create table to load csv from the nyc open data portal text
 DROP TABLE IF EXISTS dcp_melissa_september2017;
 CREATE TABLE dcp_melissa_september2017 (
-	rbdi text,
-	title text,
 	address text,
+	suite text,
 	city text,
-	state text,
+	st text,
 	zip text,
 	plus4 text,
-	walkseq text,
-	crrt text,
-	endorse text,
-	city_rural text,
-	dpb text,
-	dpbc text,
-	prename text,
-	firstname text,
-	initial text,
-	lastname text,
-	postname text,
-	nameslug text,
-	updated text,
-	vacant text
+	rbdi text,
+	updatedate text,
+	crrt text
 );
