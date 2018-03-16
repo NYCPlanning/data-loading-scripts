395211734080144c986de29c40b4162ff4cce8cf
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2018-03-16 17:24:21 -0400

    fixing fields in data loading script

--- datasets/dycd_facilities_afterschoolprograms/after.sql
+++ datasets/dycd_facilities_afterschoolprograms/after.sql
@@ -1,2 +1,2 @@
 ALTER TABLE dycd_facilities_afterschoolprograms ADD COLUMN hash text;
-UPDATE dycd_facilities_afterschoolprograms SET hash = md5(CAST((PROGRAM_TYPE,PROGRAM_SITE_NAME,BOROUGH_COMMUNITY,AGENCY,Contact_Number,GradeLevel_AgeGroup,Location1,Postcode,Latitude,Longitude,Community_Board,Council_District,Census_Tract,BIN,BBL,NTA) AS text));
+UPDATE dycd_facilities_afterschoolprograms SET hash = md5(CAST((PROGRAM_TYPE,PROGRAM,SITE_NAME,BOROUGH_COMMUNITY,AGENCY,Contact_Number,GradeLevel_AgeGroup,Location1,Postcode,Latitude,Longitude,Community_Board,Council_District,Census_Tract,BIN,BBL,NTA) AS text));
