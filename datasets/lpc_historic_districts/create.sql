18ae80afbafac4723675eaa290a77776a5dc73b0
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2018-12-20 12:38:51 -0500

    updating fields

--- datasets/lpc_historic_districts/create.sql
+++ datasets/lpc_historic_districts/create.sql
@@ -1,41 +1,6 @@
 -- create table to load csv from the nyc open data portal
 DROP TABLE IF EXISTS lpc_historic_districts;
 CREATE TABLE lpc_historic_districts (
-	BIN text,
-	Borough text,
-	BBL	text,
-	the_geom text,
-	Block text,
-	Lot text,
-	Des_Addres text,
-	Circa text,
-	Date_Low text,
-	Date_High text,
-	Date_Combo text,
-	Alt_Date_1 text,
-	Alt_Date_2 text,
-	Arch_Build text,
-	Own_Devel text,
-	Alt_Arch_1 text,
-	Alt_Arch_2 text,
-	Style_Prim text,
-	Style_Sec text,
-	Style_Oth text,
-	Mat_Prim text,
-	Mat_Sec text,
-	Mat_Third text,
-	Mat_Four text,
-	Mat_Other text,
-	Use_Orig text,
-	Use_Other text,
-	Build_Type text,
-	Build_Oth text,
-	Build_Nme text,
-	Notes text,
-	Hist_Dist text,
-	LM_Orig text,
-	LM_New text,
-	Shape_Leng text,
-	Shape_Area  text
+BIN text,the_geom text,BBL text,OBJECTID text,Borough text,Block text,Lot text,Des_Addres text,Circa text,Date_Low text,Date_High text,Date_Combo text,Alt_Date_1 text,Alt_Date_2 text,Arch_Build text,Own_Devel text,Alt_Arch_1 text,Alt_Arch_2 text,Style_Prim text,Style_Sec text,Style_Oth text,Mat_Prim text,Mat_Sec text,Mat_Third text,Mat_Four text,Mat_Other text,Use_Orig text,Use_Other text,Build_Type text,Build_Oth text,Build_Nme text,Notes text,Hist_Dist text,LM_Orig text,LM_New text,Shape_Leng text,Shape_Area text
 );
 
--- datasets/lpc_landmarks/create.sql
+++ datasets/lpc_landmarks/create.sql
@@ -1,30 +1,6 @@
 -- create table to load csv from the nyc open data portal
 DROP TABLE IF EXISTS lpc_landmarks;
 CREATE TABLE lpc_landmarks (
-	BIN_NUMBER text,
-	OBJECTID text,
-	BBL text,
-	the_geom text,
-	BoroughID text,
-	BLOCK text,
-	LOT text,
-	LP_NUMBER text,
-	LM_NAME text,
-	PLUTO_ADDR text,
-	DESIG_ADDR text,
-	PUBLIC_HEA text,
-	LM_TYPE text,
-	HIST_DISTR text,
-	OTHER_HEAR text,
-	BOUNDARIES text,
-	MOST_CURRE text,
-	STATUS text,
-	LAST_ACTIO text,
-	STATUS_NOT text,
-	COUNT_BLDG text,
-	NON_BLDG text,
-	VACANT_LOT text,
-	SECND_BLDG text,
-	Des_Date text,
-	Cal_Date text
+BIN_NUMBER text,BoroughID text,the_geom text,BBL text,BLOCK text,LOT text,LP_NUMBER text,LM_NAME text,PLUTO_ADDR text,DESIG_ADDR text,PUBLIC_HEA text,LM_TYPE text,HIST_DISTR text,OTHER_HEAR text,BOUNDARIES text,MOST_CURRE text,STATUS text,LAST_ACTIO text,COUNT_BLDG text,NON_BLDG text,VACANT_LOT text,SECND_BLDG text,DESDATE text,CALDATE text
 );
+
