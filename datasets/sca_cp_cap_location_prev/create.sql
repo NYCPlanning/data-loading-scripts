331807444960282fa61e1bb6549412344f97585c
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2019-03-15 12:12:40 -0400

    adding and updating SCA CP data loading scripts

--- datasets/sca_cp_cap_location_prev/create.sql
+++ datasets/sca_cp_cap_location_prev/create.sql
@@ -4,16 +4,7 @@ CREATE TABLE sca_cp_cap_location_prev (
 	DISTRICT text,
 	SCHOOL text,
 	BOROUGH	text,
-	LOCATION text,
-	Postcode text,
-	Latitude text,
-	Longitude text,
-	CommunityBoard text,
-	CouncilDistrict text, 	
-	CensusTract text,	
-	BIN	text,
-	BBL	text,
-	NTA text
+	LOCATION text
 );
 
 
