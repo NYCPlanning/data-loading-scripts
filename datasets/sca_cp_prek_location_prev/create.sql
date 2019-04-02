368ec59adcb1817fd3b2368d104a936c5c1a4fd2
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2019-04-02 13:15:44 -0400

    removing spaces from field names

--- datasets/sca_cp_prek_location_prev/create.sql
+++ datasets/sca_cp_prek_location_prev/create.sql
@@ -1,5 +1,5 @@
 -- create table to load csv from the nyc open data portal
 DROP TABLE IF EXISTS sca_cp_prek_location_prev;
 CREATE TABLE sca_cp_prek_location_prev (
-DISTRICT text,SCHOOL text,BOROUGH text,LOCATION text,Postcode text,Latitude text,Longitude text,Community Board text,Council District text,Census Tract text,BIN text,BBL text,NTA text
+DISTRICT text,SCHOOL text,BOROUGH text,LOCATION text,Postcode text,Latitude text,Longitude text,CommunityBoard text,CouncilDistrict text,CensusTract text,BIN text,BBL text,NTA text
 );
