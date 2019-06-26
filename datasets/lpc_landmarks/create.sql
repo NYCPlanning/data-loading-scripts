3ad82fdca1cbc7a4df517ea6f3c70d3bcf734a51
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2019-06-26 14:48:53 -0400

    updating to latest schema

--- datasets/lpc_landmarks/create.sql
+++ datasets/lpc_landmarks/create.sql
@@ -1,6 +1,5 @@
 -- create table to load csv from the nyc open data portal
 DROP TABLE IF EXISTS lpc_landmarks;
 CREATE TABLE lpc_landmarks (
-BIN_NUMBER text,BoroughID text,the_geom text,BBL text,BLOCK text,LOT text,LP_NUMBER text,LM_NAME text,PLUTO_ADDR text,DESIG_ADDR text,PUBLIC_HEA text,LM_TYPE text,HIST_DISTR text,OTHER_HEAR text,BOUNDARIES text,MOST_CURRE text,STATUS text,LAST_ACTIO text,COUNT_BLDG text,NON_BLDG text,VACANT_LOT text,SECND_BLDG text,DESDATE text,CALDATE text
+the_geom text,BIN_NUMBER text,OBJECTID text,BBL text,BoroughID text,BLOCK text,LOT text,LP_NUMBER text,RELATED_LP text,CHANGED_LP text,LM_NAME text,OTHER_NAME text,PLUTO_ADDR text,DESIG_ADDR text,DESIG_DATE text,PUBLIC_HEA text,LM_TYPE text,HIST_DISTR text,OTHER_HEAR text,BOUNDARIES text,NOTES text,MOST_CURRE text,STATUS text,LAST_ACTIO text,STATUS_NOT text,COUNT_BLDG text,NON_BLDG text,VACANT_LOT text,SECND_BLDG text,CD text,CCD text,NO_COUNT text,CALEN_DATE text
 );
-
